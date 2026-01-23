a89026596c1124205a3b1b6bb38c8d7b71bc5fd1
Girdhar <greatgmang@gmail.com>
2026-01-23 07:58:50 +0530

    add mcq submit route

--- src/routes/contest.route.ts
+++ src/routes/contest.route.ts
@@ -3,7 +3,7 @@ import { prisma } from '../lib/prisma.js'
 import { createContestSchema } from '../schema/contestSchema.js'
 import { ZodError } from 'zod'
 import { errorResponse, successResponse } from '../lib/response.js'
-import { mcqSchema } from '../schema/mcqSchema.js'
+import { mcqSchema, selectedOptionSchema } from '../schema/mcqSchema.js'
 const router = express.Router()
 
 router.post('/',async(req:Request, res:Response)=>{
@@ -187,6 +187,101 @@ router.post('/:contestId/mcq',async(req:Request,res:Response)=>{
     }
     
     
+})
+
+router.post('/:contestId/mcq/:questionId/submit',async(req:Request,res:Response)=>{
+    try {
+        
+    const contestId = Number(req.params.contestId)
+    const questionId = Number(req.params.questionId)
+
+    const userId = (req as any).user.id
+    const user = await prisma.user.findFirst({
+        where:{
+            id:userId
+        }
+    })
+    if(user?.role === 'creator'){
+        return res.status(403).json({
+            "success": false,
+            "data": null,
+            "error": "FORBIDDEN"
+        })
+    }
+    const isSubmitted = await prisma.mcq_submission.findFirst({
+        where:{
+            userId,
+            questionId
+        }
+    })
+    if(isSubmitted){
+        return res.status(400).json({
+            "success": false,
+            "data": null,
+            "error": "ALREADY_SUBMITTED"
+        })
+    }
+
+    const questionExists = await prisma.mcq_question.findFirst({
+        where:{
+            id:questionId
+        }
+    })
+    if(!questionExists){
+        return res.status(404).json({
+            "success": false,
+            "data": null,
+            "error": "QUESTION_NOT_FOUND"
+        })
+    }
+
+    if(!req.body){
+        return res.status(400).json({
+            "success": false,
+            "data": null,
+            "error": "INVALID_REQUEST"
+        })
+    }
+    const validatedData = selectedOptionSchema.safeParse(req.body)
+    if(!validatedData.success){
+        return res.status(400).json({
+            "success": false,
+            "data": null,
+            "error": "INVALID_REQUEST"
+        })
+    }
+    const {selectedOptionIndex} = validatedData.data
+
+    const isCorrect = selectedOptionIndex===questionExists.correctOptionIndex
+
+    const pointsEarned = isCorrect? questionExists.points : 0
+
+    const submission = await prisma.mcq_submission.create({
+        data:{
+            isCorrect,
+            selectedOptionIndex,
+            pointsEarned,
+            questionId,
+            userId
+        }
+    })
+    
+    res.status(201).json({
+        "success": true,
+        "data": {
+            "isCorrect": submission.isCorrect,
+            "pointsEarned": submission.pointsEarned
+        },
+        "error": null
+    })
+
+    } catch (error) {
+        console.error('Error in submitting mcq\n',error)
+        res.status(500).json({
+            message:'Internal server error'
+        })
+    }
+
 })
 
 export default router
--- src/schema/mcqSchema.ts
+++ src/schema/mcqSchema.ts
@@ -3,7 +3,12 @@ import {z} from 'zod'
 export const mcqSchema = z.object({
     questionText : z.string(),
     options:z.array(z.string()).length(4),
-    correctOptionIndex: z.number().min(1).max(4),
+    correctOptionIndex: z.number().min(0).max(4),
     points:z.number()
+})
+.strict()
+
+export const selectedOptionSchema = z.object({
+    selectedOptionIndex : z.number().nonnegative().max(3)
 })
 .strict()
